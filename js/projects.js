aca8dd5036c59254fe5a4b3c997b1cb86d54ae7b
Saud <saudd2gh@gmail.com>
2019-04-26 15:05:29 +0400

    multiple projects added

--- js/projects.js
+++ js/projects.js
@@ -4,7 +4,25 @@ const projects = [
     description:
       "Lorem ipsum dolor sit amet, vidisse corpora intellegebat ne pri. Eam at quod iudicabit definitiones, at graeci vidisse oblique has.",
     imgSrc: "../images/smartmockups_jux4p0lc.png",
+    skills: ["HTML", "Javascript", "CSS", "Materialize CSS"],
+    repo: "travel_repo",
+    demolink: "travel_demo"
+  },
+  {
+    title: "DevDevil Electronics Blog",
+    description:
+      "Lorem ipsum dolor sit amet, vidisse corpora intellegebat ne pri. Eam at quod iudicabit definitiones, at graeci vidisse oblique has.",
+    imgSrc: "../images/smartmockups_jux4p0lc.png",
     skills: ["WordPress", "HTML", "Javascript", "CSS", "Materialize CSS"],
+    repo: "devdevil_repo",
+    demolink: "devdeil_demo"
+  },
+  {
+    title: "Fourier Series Calculator",
+    description:
+      "Lorem ipsum dolor sit amet, vidisse corpora intellegebat ne pri. Eam at quod iudicabit definitiones, at graeci vidisse oblique has.",
+    imgSrc: "../images/smartmockups_jux4p0lc.png",
+    skills: ["HTML", "Javascript", "CSS", "Materialize CSS"],
     repo: "#",
     demolink: "#"
   }
