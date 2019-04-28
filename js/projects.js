a0b50b8a83b4965d32db1cac70add44fc1b2f2f9
Saud <saudd2gh@gmail.com>
2019-04-28 12:21:40 +0400

    minor mods

--- js/projects.js
+++ js/projects.js
@@ -15,15 +15,16 @@ const projects = [
     imgSrc: "../images/smartmockups_jux4p0lc.png",
     skills: ["WordPress", "HTML", "Javascript", "CSS", "Materialize CSS"],
     repo: "devdevil_repo",
-    demolink: "devdeil_demo"
-  },
-  {
-    title: "Fourier Series Calculator",
-    description:
-      "Lorem ipsum dolor sit amet, vidisse corpora intellegebat ne pri. Eam at quod iudicabit definitiones, at graeci vidisse oblique has.",
-    imgSrc: "../images/smartmockups_jux4p0lc.png",
-    skills: ["HTML", "Javascript", "CSS", "Materialize CSS"],
-    repo: "#",
-    demolink: "#"
+    demolink: "https://www.devdevil.co"
   }
+  // ,
+  // {
+  //   title: "Fourier Series Calculator",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, vidisse corpora intellegebat ne pri. Eam at quod iudicabit definitiones, at graeci vidisse oblique has.",
+  //   imgSrc: "../images/smartmockups_jux4p0lc.png",
+  //   skills: ["HTML", "Javascript", "CSS", "Materialize CSS"],
+  //   repo: "#",
+  //   demolink: "#"
+  // }
 ];
