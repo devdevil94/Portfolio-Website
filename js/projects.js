f8a0f93312fe3db8b0063570d8c8d2f76b51f3d5
Saud <saudd2gh@gmail.com>
2019-04-29 21:28:37 +0400

    projects info modified

--- js/projects.js
+++ js/projects.js
@@ -2,7 +2,7 @@ const projects = [
   {
     title: "Travel Agency Website",
     description:
-      "Lorem ipsum dolor sit amet, vidisse corpora intellegebat ne pri. Eam at quod iudicabit definitiones, at graeci vidisse oblique has.",
+      "A website created for travel agencies to showcase their available packages as well as the famous attractions.",
     imgSrc: "../images/smartmockups_jux4p0lc.png",
     skills: [
       "HTML",
@@ -11,16 +11,16 @@ const projects = [
       "Responsive Design",
       "Materialize CSS"
     ],
-    repo: "travel_repo",
-    demolink: "travel_demo"
+    repo: "https://github.com/devdevil94/Omanova-Travel-Agency",
+    demolink: "https://devdevil94.github.io/Omanova-Travel-Agency/"
   },
   {
     title: "DevDevil Electronics Blog",
     description:
-      "Lorem ipsum dolor sit amet, vidisse corpora intellegebat ne pri. Eam at quod iudicabit definitiones, at graeci vidisse oblique has.",
-    imgSrc: "../images/smartmockups_jux4p0lc.png",
+      "A personal blog created with a Wordpress theme developed from scratch.",
+    imgSrc: "../images/smartmockups_jv2mq9qb.png",
     skills: ["WordPress", "HTML", "Javascript", "CSS", "Responsive Design"],
-    repo: "devdevil_repo",
+    repo: "https://github.com/devdevil94/devdevil",
     demolink: "http://devdevil.co"
   }
   // ,
